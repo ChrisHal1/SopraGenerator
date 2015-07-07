28e5e5cb1e587d9d6b2d76f9ccea6e0752ad85ee
ChrisHal1 <c.halderkisslegg@web.de>
2015-07-07 17:01:51 +0200

    Update UserRepository.java

--- src/main/java/org/hohenheim/model/repository/UserRepository.java
+++ src/main/java/org/hohenheim/model/repository/UserRepository.java
@@ -2,11 +2,24 @@ package org.hohenheim.model.repository;
 
 import org.hohenheim.model.entity.user.User;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+/* @Repository class
+ * @Last Updated: 07.07 14:24 by @Simon
+ * @Description:
+ *
+ * User Repository
+ * This Repository provides methods to manage Users within the database.
+ * 
+ *
+ */
+
+@Transactional
 @Repository
 public class UserRepository extends BaseRepository<User> {
 
-	public UserRepository(){
+	public UserRepository() {
 		super();
 	}
+
 }
