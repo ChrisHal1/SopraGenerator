a6472322a07a3e51d52a0bf5abcc2fd1c21cdff6
ChrisHal1 <c.halderkisslegg@web.de>
2015-07-07 16:50:59 +0200

    Update IndexController.java

--- src/main/java/org/hohenheim/controller/IndexController.java
+++ src/main/java/org/hohenheim/controller/IndexController.java
@@ -1,18 +1,57 @@
 package org.hohenheim.controller;
 
+import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+import org.hohenheim.model.entity.education.LGroup;
+import org.hohenheim.model.entity.user.User;
+import org.hohenheim.services.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/* @Controller Class
+ * @Last Updated: 07.07 13:03 by @Simon
+ * @Description:
+ * Index Controller class, handles requests for home(index) page.
+ */
 
 @Controller
-@RequestMapping("/")
-public class IndexController implements IController{
-    @RequestMapping(method = RequestMethod.GET)
-    public String action(){
-	
-	//TODO bentutzerkonto bei korrektheit in die session legen
-	return "index";
-    }
+public class IndexController implements IController {
 
-   
+	@Autowired
+	private GroupService groupService;
+
+	/*
+	 * landing page with automatic login.
+	 */
+	@RequestMapping(value = "/")
+	public String index(Model model) {
+
+		//this.groupService.createGroup("mygroup");
+		model.addAttribute("user", groupService.getCurrentUserObject());
+
+		return "index";
+	}
+
+	/*
+	 * Get all groups of a specific user.
+	 */
+	@ModelAttribute("userGroups")
+	public List<LGroup> getUserGroups() {
+		return this.groupService.getUsersGroups(this.groupService.getUsernameOfCurrentUser());
+	}
+
+	/*
+	 * 1. Get all groups, except those a specific user is already a member of OR
+	 * 2. If a search parameter has been specified in the request, only groups matching the pattern are returned.
+	 */
+	@ModelAttribute("openGroups")
+	public List<LGroup> getOpenGroups(HttpServletRequest request) {
+		if (request.getParameter("key") != null && request.getParameter("key") != "")
+			return this.groupService.search(request.getParameter("key"));
+		else
+			return this.groupService.getOpenGroups(this.groupService.getUsernameOfCurrentUser());
+	}
+
 }
