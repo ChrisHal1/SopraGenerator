69b5f49c68331160d2e007d134aaf08ebf97b352
ChrisHal1 <c.halderkisslegg@web.de>
2015-07-07 16:59:02 +0200

    Update BaseRepository.java

--- src/main/java/org/hohenheim/model/repository/BaseRepository.java
+++ src/main/java/org/hohenheim/model/repository/BaseRepository.java
@@ -1,54 +1,181 @@
 package org.hohenheim.model.repository;
 
+import java.lang.reflect.ParameterizedType;
+import java.util.*;
+
+import org.hibernate.*;
+import org.hibernate.criterion.SimpleExpression;
 import org.hohenheim.model.entity.BaseEntity;
-import org.hohenheim.model.io.DBConnection;
-import org.springframework.stereotype.Repository;
+import org.hohenheim.util.ORDER;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.Transactional;
+
 
 /* @Repository base class
- * @Last Updated: 25.06 15:36 by @Simon
+ * @Last Updated: 05.07 21:24 by @Simon
  * @Description:
  *
  * Repository Base class
  * This Repository is the base class for every repository. It provides some simple and often used methods
  * for managing, persisting and retrieving objects of an entity type to database. Every entity type must have
  * its own repository, inheriting from this class.
+ * This class represents the interface for the db.
  * 
  *
  */
 
+@Transactional
 @Repository
 public abstract class BaseRepository<T extends BaseEntity> implements IRepository<T> {
 
-	private DBConnection<T> db;
+	@Autowired
+	protected SessionFactory session;
+	public void setSession(SessionFactory session){ this.session = session; }
 	
 	public BaseRepository() {
-		this.db = new DBConnection<T>();
+	}
+	
+	protected Session getSession(){
+		return this.session.getCurrentSession();
+	}
+	
+	@Override
+	public String getNameOfClass(){
+		return ((Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]).getSimpleName();
+
+	}
+	
+	/*
+	 * @see org.hohenheim.model.repository.IRepository#save(org.hohenheim.model.entity.BaseEntity)
+	 */
+	@Override
+	public boolean save(T obj) throws Exception{
+		
+		return (boolean)((long)this.getSession().save(obj) > 0);
+	}
+	
+	/*
+	 * @see org.hohenheim.model.repository.IRepository#delete(org.hohenheim.model.entity.BaseEntity)
+	 */
+	@Override
+	public boolean delete(T obj) throws Exception{
+		String query = "DELETE FROM " + this.getNameOfClass() + "WHERE ID = :id";
+		int result = 0;
+		
+		try{
+			Query q = this.getSession().createQuery(query);
+			q.setParameter("id", obj.ID);
+			result = q.executeUpdate();
+		}
+		catch(HibernateException e){
+			
+		}
+		return (result != 0);
 	}
 
+	/*
+	 * @see org.hohenheim.model.repository.IRepository#runQuery(java.lang.String)
+	 */
 	@Override
-	public boolean save(T entity){
+	public List<T> runQuery(String query) throws Exception{
+		Session session = this.getSession();
+		
 		try{
-			return (this.db.saveObject(entity) != 0);
+			Query _query = session.createQuery(query);
+			return (List<T>)_query.list();
 		}
-		catch(Exception e){
-			return false;
+		catch(HibernateException e){
+		}
+		return new ArrayList<T>();
+	}
+	
+	/*
+	 * @see org.hohenheim.model.repository.IRepository#getAllObjectsFromTable(boolean)
+	 */
+	@Override
+	public List<T> getAllObjectsFromTable(ORDER order) throws Exception{
+		String query = "from " + this.getNameOfClass() + " as T";
+		
+		if(order == ORDER.CREATION_DATE)
+			query += " order by T.created asc";
+		else if(order == ORDER.POINTS)
+			query += " order by T.points desc";
+		
+		return (List<T>)this.runQuery(query);
+	}
+	
+	/*
+	 * @see org.hohenheim.model.repository.IRepository#getById(long)
+	 */
+	@Override
+	public T getById(long Id) throws Exception{
+		String query = "from " + this.getNameOfClass() + " as T where T.ID = :id";
+		List<T> result = null;
+		
+		try{
+			Query q = this.getSession().createQuery(query);
+			q.setParameter("id", Id);
+			result = (List<T>)q.list();
+			
+			if(result.size() > 0)
+				return (T)result.get(0);
+			else
+				return null;
+		}
+		catch(HibernateException e){
+			return null;
 		}
 	}
 	
+	/*
+	 * @see org.hohenheim.model.repository.IRepository#getByUniqueAttribute(java.lang.String, java.lang.String)
+	 */
 	@Override
-	public boolean delete(T entity){
+	public T getByUniqueAttribute(String attribute, String value) throws Exception{
+		String query = "from " + this.getNameOfClass() + " as T where T." + attribute + " = :val";
+		List<T> result = null;
+		
 		try{
-			return (this.db.deleteObject(entity));
+			Query q = this.getSession().createQuery(query);
+			q.setParameter("val", value);
+			result = (List<T>)q.list();
+			
+			if(result.size() > 0)
+				return (T)result.get(0);
+			else
+				return null;
 		}
-		catch(Exception e){
-			return false;
+		catch(HibernateException e){
+			return null;
 		}
 	}
 	
+	/*
+	 * @see org.hohenheim.model.repository.IRepository#runQueryWithRestrictions(java.lang.Class, java.util.List, int, int)
+	 */
 	@Override
-	public T getById(int Id){
-		//todo
-		return null;
+	public List<T> runQueryWithRestrictions(Class<T> Tclass, List<SimpleExpression> criterias, int paginate_start, int paginate_range) throws Exception{
+		
+		List<T> result = new ArrayList<T>();
+
+		try{
+			Criteria cr = this.getSession().createCriteria(Tclass);
+		
+			for(int i = 0; i < criterias.size(); i++)
+				cr.add(criterias.get(i));
+			
+			if(paginate_start > 0)
+				cr.setFirstResult(paginate_start);
+			if(paginate_range != -1)
+				cr.setMaxResults(paginate_range);
+			
+			result = (List<T>)cr.list();
+		}
+		catch(HibernateException e){
+			
+		}
+		return result;
 	}
 
 }
