035809af0e39b7b8453050fe661eca49c78fed79
ChrisHal1 <c.halderkisslegg@web.de>
2015-07-07 17:06:13 +0200

    Update TestController.java

--- src/main/java/org/hohenheim/TestController.java
+++ src/main/java/org/hohenheim/TestController.java
@@ -1,20 +1,27 @@
 package org.hohenheim;
 
+import org.hibernate.SessionFactory;
+import org.hohenheim.model.entity.user.User;
+import org.hohenheim.services.BaseService;
+import org.hohenheim.util.ROLE;
+import org.hohenheim.model.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/hep")
 public class TestController {
-    @RequestMapping(method = RequestMethod.GET)
+
+    @RequestMapping(value="", method = RequestMethod.GET)
     public String hi(
 	    @RequestParam(value = "name", required = false, defaultValue = "World") String name,
 	    Model model) {
-	model.addAttribute("name", name);
-	System.out.println("falscherController");
+	
+    
+	User usr = new User("Test", "hallo", "simon.schoenwaelder@mx.de", ROLE.ROLE_ADMIN);
+	//model.addAttribute("name", t);
 	return "index";
     }
 }
