b30ca198cf58ca54f20a81c7b32266b03e07e092
ChrisHal1 <c.halderkisslegg@web.de>
2015-07-08 13:36:53 +0200

    gruppe und profil bearbeitet

--- src/main/java/org/hohenheim/controller/GruppenController.java
+++ src/main/java/org/hohenheim/controller/GruppenController.java
@@ -1,14 +1,23 @@
 package org.hohenheim.controller;
 
+import java.util.List;
+
+import org.hohenheim.model.entity.education.LGroup;
+import org.hohenheim.model.repository.UserRepository;
 import org.hohenheim.services.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GruppenController implements IController {
+    
+    @Autowired
+	GroupService groupService;
 
     @RequestMapping(value = "/gruppeAnlegenImProfil", method = RequestMethod.GET)
     public String gruppeAnlegenImProfil() {
@@ -16,12 +25,12 @@ public class GruppenController implements IController {
 	return "gruppeAnlegen";
     }
 
-    @RequestMapping(value = "/gruppeAnlegen", method = RequestMethod.GET)
-    public String gruppeAnlegen() {
-	// TODO Auto-generated method stub
-	// gruppe der db hinzufügen, neue gruppe anzeigen
-	return "gruppe";
-    }
+//    @RequestMapping(value = "/gruppeAnlegen", method = RequestMethod.GET)
+//    public String gruppeAnlegen() {
+//	// TODO Auto-generated method stub
+//	// gruppe der db hinzufügen, neue gruppe anzeigen
+//	return "gruppe";
+//    }
 
     @RequestMapping(value = "/gruppeAnlegenImProfil", method = RequestMethod.POST)
     public String gruppeAnlegenPost(@ModelAttribute("gruppenname") String gruppenname,
@@ -29,11 +38,21 @@ public class GruppenController implements IController {
 	    @ModelAttribute("lernfach") String lernfach,
 	    @ModelAttribute("themengebiet") String themengebiet,
 	    BindingResult resutlt) {
-	// TODO Auto-generated method stub
+	// TODO gruppe anlegen mit service
 	System.out.println(gruppenname);
 	System.out.println(beschreibung);
 	System.out.println(lernfach);
 	System.out.println(themengebiet);
 	return "gruppe";
     }
+    
+    @RequestMapping(value = "/diskussionErstellen", method = RequestMethod.POST)
+    public String diskussionErstellen(@ModelAttribute("titleOfDiscussion") String titleOfDiscussion,
+	    BindingResult resutlt) {
+	// TODO neuer GroupPost
+	
+	return "gruppe";
+    }
+    
+   
 }
--- src/main/java/org/hohenheim/controller/HomeController.java
+++ src/main/java/org/hohenheim/controller/HomeController.java
@@ -1,19 +1,55 @@
 package org.hohenheim.controller;
 
+import java.util.List;
+
+import org.hohenheim.model.entity.education.LGroup;
+import org.hohenheim.model.entity.posting.Post;
+import org.hohenheim.model.entity.posting.UserPost;
+import org.hohenheim.services.GroupService;
+import org.hohenheim.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController implements IController{
-    @RequestMapping(method = RequestMethod.GET, value = "/profil")
-    public String action(){
+    
+    @Autowired
+    GroupService groupService;
+    @Autowired 
+    UserService userService;
+    
+//    @RequestMapping(method = RequestMethod.GET, value = "/profil")
+//    public String action(){
+//	
+//	//TODO: checken ob benutzerkonto in der session liegt
+//	System.out.println("hi");
+//	
+//	return "profil";
+//    }
+    
+    @RequestMapping(method=RequestMethod.GET, value="/profil")
+   	public ModelAndView showGroups() {
+   		
+   		//List<LGroup> groups = groupService.getUsersGroups(groupService.getUsernameOfCurrentUser());
+//   		List<Post> posts = userService.getCurrentUserObject().createdPosts;
+   		ModelAndView mv = new ModelAndView();
+   	//	mv.addObject("groups", groups);
+//   		mv.addObject("posts", posts);
+   		return mv;
+   	}
+    
+    @RequestMapping(method=RequestMethod.GET, value="/gruppenwahl/${aktuelleGruppe.name}")
+	public String gruppeAusgewaehlt(@PathVariable("name") String name) {
 	
-	//TODO: checken ob benutzerkonto in der session liegt
-	System.out.println("hi");
-	
-	return "profil";
-    }
+		
+		System.out.println("gruppe ausgewaehlt: "+name);
+		//bestimmte gruppe auswählen
+		return "gruppe";
+	}
     
     @RequestMapping(value="/suche")
     public String suche() {
