94e69237c6a0fef42c120de16b33f698fe7993f3
ChrisHal1 <c.halderkisslegg@web.de>
2015-07-08 11:34:11 +0200

    gruppe anlegen importiert jetzt die eingegebenen strings und schreibt sie auf konsole

--- src/main/java/org/hohenheim/controller/GruppenController.java
+++ src/main/java/org/hohenheim/controller/GruppenController.java
@@ -1,27 +1,39 @@
 package org.hohenheim.controller;
 
+import org.hohenheim.services.GroupService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class GruppenController implements IController {
 
-    @RequestMapping("/gruppeAnlegenImProfil")
+    @RequestMapping(value = "/gruppeAnlegenImProfil", method = RequestMethod.GET)
     public String gruppeAnlegenImProfil() {
 	// TODO Auto-generated method stub
 	return "gruppeAnlegen";
     }
 
-    @RequestMapping("/gruppeAnlegen")
+    @RequestMapping(value = "/gruppeAnlegen", method = RequestMethod.GET)
     public String gruppeAnlegen() {
 	// TODO Auto-generated method stub
-	//gruppe der db hinzufügen, neue gruppe anzeigen
+	// gruppe der db hinzufügen, neue gruppe anzeigen
 	return "gruppe";
     }
-    @RequestMapping("/gruppeAnlegenPOST")
-    public String gruppeAnlegenPost() {
+
+    @RequestMapping(value = "/gruppeAnlegenImProfil", method = RequestMethod.POST)
+    public String gruppeAnlegenPost(@ModelAttribute("gruppenname") String gruppenname,
+	    @ModelAttribute("beschreibung") String beschreibung,
+	    @ModelAttribute("lernfach") String lernfach,
+	    @ModelAttribute("themengebiet") String themengebiet,
+	    BindingResult resutlt) {
 	// TODO Auto-generated method stub
-System.out.println("tesst");
+	System.out.println(gruppenname);
+	System.out.println(beschreibung);
+	System.out.println(lernfach);
+	System.out.println(themengebiet);
 	return "gruppe";
     }
 }
